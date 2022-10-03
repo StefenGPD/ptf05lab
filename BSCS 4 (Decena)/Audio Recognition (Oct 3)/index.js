bfa472c943669a3a96d3d16fd84b327b29ba3789
StefenGPD <67962272+StefenGPD@users.noreply.github.com>
2022-10-03 11:07:20 +0800

    Files added

---
+++ BSCS 4 (Decena)/Audio Recognition (Oct 3)/index.js
@@ -0,0 +1,22 @@
+let recognizer;
+
+function predictWord(){
+    const words = recognizer.wordLabels();
+    console.log(words);
+
+    recognizer.listen(({scores}) => {
+        scores = Array.from(scores).map((s,i) => ({score: s, word: words[i]}));
+        scores.sort((s1,s2) => s2.score - s1.score);
+
+        document.querySelector('#console').textContent = scores[0].word.toUpperCase();
+    }, (probabilityThreshold = 0.90));
+    
+}
+
+async function app() {
+    recognizer = speechCommands.create('BROWSER_FFT');
+    await recognizer.ensureModelLoaded();
+    predictWord();
+}
+
+app()
